d9243364f705f92489af0a3c97ba464f2f3f17f0
MrSeryLol <psygentelmen014@gmail.com>
2023-10-02 09:54:50 +0300

    Practice 6 is ready

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,12 +17,21 @@ fun main(args: Array<String>) {
 //    brunoSong.printDescription()
 //    println(brunoSong.isPopular)
 
-    //Practice 5
-    val amanda = Person("Amanda", 33, "play tennis", null)
-    val atiqah = Person("Atiqah", 28, "climb", amanda)
+//    //Practice 5
+//    val amanda = Person("Amanda", 33, "play tennis", null)
+//    val atiqah = Person("Atiqah", 28, "climb", amanda)
+//
+//    amanda.showProfile()
+//    atiqah.showProfile()
 
-    amanda.showProfile()
-    atiqah.showProfile()
+    //Practice 6
+    val newFoldablePhone = FoldablePhone()
+
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
+    newFoldablePhone.unfold()
+    newFoldablePhone.switchOn()
+    newFoldablePhone.checkPhoneScreenLight()
 
 
 }
