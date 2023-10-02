4009aadfc7105b42577d71caefa99773bed318e9
MrSeryLol <psygentelmen014@gmail.com>
2023-10-02 09:33:41 +0300

    Practice 2 is ready

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 fun main(args: Array<String>) {
 
-    val instance = MobileNotifications()
+//    //Practice 1
+//    val instance = MobileNotifications()
+//    instance.progStart()
+
+    //Practice 2
+    val instance = MovieTicketPrice()
     instance.progStart()
 
 
